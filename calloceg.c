6f206a5f5278af4a5bff41e709c0a92c329ff492
Darakshan kamal <darakshanskamal282@gmail.com>
2026-04-14 13:13:01 +0530

    Hey! today I'm learning Dynamic Memory Allocation

---
+++ calloceg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int *ptr;
+    int n;
+    printf("Enter the size of the array \n ");
+    scanf("%d",&n);
+    ptr=(int*) calloc (n,sizeof(int));
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("enter the value %d \n",i);
+    //     scanf("%d",&ptr[i]);
+        
+    // }
+    for (int i = 0; i < n; i++)
+    {
+        printf("The value at index %d is %d \n",i,ptr[i]);
+    }
+    
+    return 0;
+}
